77818a382553115e90f538b5e3905be2066fc5ea
Diego Czajka <diego.czajka@gmail.com>
2024-07-27 15:29:45 -0300

    feat: doc config

--- app/api/index.ts
+++ app/api/index.ts
@@ -4,10 +4,9 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import * as dotenv from "dotenv";
 import express, { Application } from "express";
-import dbClient from "./services/sequlizeCon";
-import { userRouter } from "./routes/userRoutes";
-import { productsRouter } from "./routes/productRoutes";
-import { vercelRouter } from "./routes/vercelFunctionsRoutes";
+import dbClient from "../services/sequlizeCon";
+import { userRouter } from "../routes/userRoutes";
+import { productsRouter } from "../routes/productRoutes";
 
 dotenv.config();
 
@@ -21,11 +20,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 async () => await dbClient.sync();
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productsRouter);
-app.use("/api/v1/teste", vercelRouter);
-app.use("/api/v1/",function GET(request: Request) {
-  return new Response(`Hello from World`);
-});
+
+
 
 app.listen(PORT);
 
-export default app;
+module.exports = app;
--- app/routes/vercelFunctionsRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { teste } from "../controllers/vercelFunctions";
-const vercelRouter = Router();
-
-vercelRouter.get("/", teste);
-
-export { vercelRouter };
